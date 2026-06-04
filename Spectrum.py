3e8ddfc4b76d9ec4e07da5d6ac3bb4873e026826
RubenDelahaij <rubendelahaij@gmail.com>
2026-06-04 13:51:17 +0200

    Add files via upload
    These are the code to visualise the absorbtion spectrum and the data used in the process

---
+++ Spectrum.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = []
+y = []
+
+with open('datta.csv', 'r', newline='') as file:
+    next(file)
+    for line in file:
+        line_splitted = line.split(',')
+        x.append(float(line_splitted[3]))
+        y.append(float(line_splitted[4]))
+
+y_top = max(y)
+y_min = min(y)
+
+# Gebruik index() om de bijbehorende x te vinden
+x_top = x[y.index(y_top)]
+x_min = x[y.index(y_min)]
+
+print(x_top)
+print(x_min)
+print(y_top)
+print(y_min)
+
+a = (y_top - y_min) / (x_top - x_min)
+print(a)
+b = y_top - a * x_top
+
+y_line = []
+for i in x:
+    y_line.append(a* i + b)
+
+y_norm = []
+for i in range (0, len(y_line)):
+    y_norm.append(y_line[i] - y[i])
+
+
+print(y_line)
+plt.figure()  # was plt.figure zonder ()
+plt.plot(x, y)
+plt.plot(x, y_line)
+plt.show()
+
+
+plt.figure()
+plt.plot(x, y_norm)
+plt.xlim(0.315, 0.36)
+plt.show()
