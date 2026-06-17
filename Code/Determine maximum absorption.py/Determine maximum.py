8946b12afc5f16c3762798b0e78dbdfb21ac8ad0
RubenDelahaij <rubendelahaij@gmail.com>
2026-06-17 15:38:05 +0200

    added code to determine that the second peak was the highest

--- Code/Determine maximum absorption.py/Determine maximum.py
+++ Code/Determine maximum absorption.py/Determine maximum.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 x = []
 y = []
 
-with open(r'C:\Users\ruben\Documents\GitHub\Non-Relativistic-saturation-spectroscopy\Code\F0000CH1.CSV', 'r') as file:
+with open('F0000CH1.CSV', 'r', newline='') as file:
     next(file)
     for line in file:
         line_splitted = line.split(',')
-        x.append(float(line_splitted[2]))
-        y.append(float(line_splitted[3]))
+        if float(line_splitted[3]) > 0.52 and float(line_splitted[4]) < 0.555:
+            x.append(float(line_splitted[3]))
+            y.append(float(line_splitted[4]))
 
 y_top = max(y)
 y_min = min(y)
@@ -45,5 +46,5 @@ plt.show()
 
 plt.figure()
 plt.plot(x, y_norm)
-plt.xlim(0.315, 0.36)
+plt.xlim(0.525, 0.555)
 plt.show()
